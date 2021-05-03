3e5b5076b364c05284dde1ca9aec8fe80f7358c2
kade <adg0807@naver.com>
2021-05-03 23:32:39 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// const _itemContainer = document.querySelector('.item__container');
+
+// let itemText = "";
+
+// _itemContainer.addEventListener('mouseover', e => {
+//   changeText(e);
+// });
+
+// _itemContainer.addEventListener('mouseout', e => {
+//   returnText(e);
+// });
+
+// function changeText(e) {
+//   itemText = e.target.closest('.item-text').innerText;
+//   e.target.closest('.item-text').innerText = "Let's Go";
+// }
+
+// function returnText(e) {
+//   e.target.closest('.item-text').innerText = itemText;
+// }
